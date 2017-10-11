527896ff5bf7ce71ae7ade137476017144b654da
Tayo Carvalhal <lucarv@microsoft.com>
2017-10-11 11:45:36 +0200

    added features for displaying orders list

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+/**
+ * Copyright (c) Microsoft Corporation
+ *  All Rights Reserved
+ *  MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the 'Software'), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+'use strict';
+
+/******************************************************************************
+ * Module dependencies.
+ *****************************************************************************/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+  name: 'Microsoft OIDC Example Web Application'
+  });
+
 var index = require('./routes/index');
 var som = require('./routes/service_order');
-
 var app = express();
 
 // view engine setup
@@ -16,7 +50,6 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/service_order.js
+++ routes/service_order.js
@@ -1,14 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var jsonfile = require('jsonfile');
+var somFile = 'som.json';
+var orderArray = [];
+
+jsonfile.readFile(somFile, function (err, obj) {
+  if (obj) {
+    orderArray = obj;
+    for (var i = 0; i < obj.length; i++) {
+      switch (obj[i].type) {
+        case 'system':
+          sysArray.push(obj[i]);
+          break;
+        case 'tag':
+          tagArray.push(obj[i]);
+          break;
+        case 'property':
+          propArray.push(obj[i]);
+          break;
+      }
+    }
+  }
+})
 
 /* GET orders listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  console.log(req.query.agentId);
+  var tasks = [];
+  var found = orderArray.filter(function (item) { return item.agentId === req.query.agentId; });
+  found.forEach(function (element) {
+    console.log(element.agentId)
+    tasks.push(element.orderSummary)
+  })
+
+  if (tasks.length > 0)
+    res.send({ tasks });
+  else
+    res.send({ 'order': 'no order in backlog' });
+
 });
 
 /* POST service order. */
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function (req, res, next) {
+  var order = {};
+
+  order['agentId'] = req.body.agentId;
+  order['orderNumber'] = req.body.orderNumber;
+  order['orderSummary'] = req.body.orderSummary;
+
+  orderArray.push(order);
+
+  jsonfile.writeFile(somFile, orderArray, function (err) {
+    if (err)
+      console.error(err);
+    else
+      console.log('service order written to file');
+  })
+  res.send(req.body);
+
 });
 
 module.exports = router;
