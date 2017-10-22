6ed6407222310bd46f121153bd163c583df65b97
Tayo Carvalhal <lucarv@microsoft.com>
2017-10-22 08:20:45 +0200

    alerting agent via twilio SMS

--- routes/service_order.js
+++ routes/service_order.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
 var jsonfile = require('jsonfile');
 var somFile = 'som.json';
 var orderArray = [];
+
+var twilio = require('twilio');
+var accountSid = 'AC32f7569dbb30616afb3189b17099e548'; // Your Account SID from www.twilio.com/console
+var authToken = 'd0e1ccc59d34ca53b38bf068636c92a2';   // Your Auth Token from www.twilio.com/console
+var client = new twilio(accountSid, authToken);
 
 jsonfile.readFile(somFile, function (err, obj) {
   if (obj) {
@@ -22,6 +28,18 @@ jsonfile.readFile(somFile, function (err, obj) {
     }
   }
 })
+
+var alertAgent = function (order) {
+  client.messages.create({
+    body: 'DLMS Provisioning new order ' + order.orderId +  'created ',
+    to: '+46734083277',  // Text this number
+    from: '+46765193249' // From a valid Twilio number
+  })
+    .then((message) => console.log(message.sid));
+
+  console.log('alerting agent');
+  return 'agent ' + order.agentId + ' alerted via sms';
+}
 
 /* GET orders listing. */
 router.get('/', function (req, res, next) {
@@ -45,18 +63,22 @@ router.post('/', function (req, res, next) {
   var order = {};
 
   order['agentId'] = req.body.agentId;
-  order['orderNumber'] = req.body.orderNumber;
+  order['orderId'] = req.body.orderId;
   order['orderSummary'] = req.body.orderSummary;
-
   orderArray.push(order);
 
   jsonfile.writeFile(somFile, orderArray, function (err) {
     if (err)
-      console.error(err);
-    else
+      res.send({ 'result': 'something went wrong: ' + JSON.stringify(err) });
+    else {
       console.log('service order written to file');
+      var sms = alertAgent(order);
+      console.log('agent alerting result: ' + sms)
+      res.send({ 'result': sms });
+    }
   })
-  res.send(req.body);
+
+
 
 });
 
