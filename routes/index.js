d839b50c9347fa9a11c56243b41c579416993680
Tayo Carvalhal <lucarv@microsoft.com>
2017-11-14 10:59:17 +0100

    removed redis, added request

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 'use strict';
+const request = require('request');
+var url = 'http://newdeviceapi20171102033605.azurewebsites.net/api/values'
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -8,6 +12,7 @@ var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConn
 var Client = require('azure-iot-device').Client;
 var Protocol = require('azure-iot-device-mqtt').Mqtt;
 
+/*
 var redis = require('redis');
 var redis_client = redis.createClient(6380, 'mnrg.redis.cache.windows.net',
   {
@@ -17,9 +22,10 @@ var redis_client = redis.createClient(6380, 'mnrg.redis.cache.windows.net',
 redis_client.on('connect', function () {
   console.log('connected');
 });
+*/
 
 var client;
-var hubCS = 'HostName=dlms-luca.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ltpaai5Vzsv12qcJfB7FMV4+1XPs4Z14qcdIsFHx6/g=  '
+var hubCS = 'HostName=dlms-luca.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ltpaai5Vzsv12qcJfB7FMV4+1XPs4Z14qcdIsFHx6/g='
 var registry = iothub.Registry.fromConnectionString(hubCS);
 var hubName = hubCS.substring(0, hubCS.indexOf(';'));
 
@@ -31,9 +37,9 @@ function printDeviceInfo(err, deviceInfo, res) {
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  redis_client.del('meterId', function(err, reply) {
+  redis_client.del('meterId', function (err, reply) {
     console.log(reply);
-});
+  });
   res.render('index', { title: 'DLMS Provisioning Back End' });
 });
 
@@ -49,16 +55,45 @@ router.post('/', function (req, res, next) {
       registry.get(device.deviceId, printDeviceInfo);
 
     var devKey;
-    res.setHeader('Content-Type', 'application/json');
 
     if (deviceInfo) {
       devKey = deviceInfo.authentication.symmetricKey.primaryKey;
-      console.log(devKey);
+      console.log('device created, devKey: ' + devKey);
 
-      redis_client.set(meterId, devKey, function(err, reply) {
-        console.log(reply);
-    });
+      var jsonData = {
+        ID: meterId,
+        Connstr: devKey
+      }
 
+      const options = {
+        method: 'post',
+        body: jsonData,
+        json: 'true',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: url
+      }
+
+      
+      request(options, function (err, res, body) {
+        if (err) {
+          console.error('error posting json: ', err)
+          throw err
+        }
+        var headers = res.headers
+        var statusCode = res.statusCode
+        console.log('headers: ', headers)
+        console.log('statusCode: ', statusCode)
+        console.log('body: ', body)
+      })
+
+      
+      /*
+            redis_client.set(meterId, devKey, function(err, reply) {
+              console.log(reply);
+          });
+      */
 
       var opRes = { 'result': 'meter ' + meterId + ' added to registry' }
       res.send(JSON.stringify(opRes));
@@ -91,5 +126,6 @@ router.delete('/', function (req, res, next) {
   });
 
 });
+
 
 module.exports = router;
